7a2423bef605213f56fa39b648ebbb88b3804c31
elderookie <suyama@mx4.mesh.ne.jp>
2025-01-21 16:23:22 +0900

    title 1.6

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,7 +7,7 @@
     <title>ToDo App</title>
 </head>
 <body>
-    <h1>ToDo リスト Version1.5</h1>
+    <h1>ToDo リスト Version1.6</h1>
     <form action="/tasks" method="POST">
         @csrf
         <input type="text" name="title" placeholder="新しいタスクを追加">
